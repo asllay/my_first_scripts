6dbd9b9127f22e35e066b283c0eaf75fd30e6f5a
Asia Szw <szwiec.joanna@gmail.com>
2017-09-16 15:19:13 +0200

    pierwszy komentarz do zaladowania

--- kalkulator.py
+++ kalkulator.py
@@ -2,3 +2,4 @@ print('Cześć, jestem kalkulatorem. Podam Ci średnią dwóch liczb')
 a = input('Podaj pierwszą liczbę:')
 b = input('Podaj drugą liczbę:')
 print((float(a) + float(b))/2)
+'''to jest moj komentarz, poniewaz chce zobaczyc zmiany'''
